e51d0220cf1b1bd760a2b1c287a440a27c2d077a
Owen <owenyangg@gmail.com>
2016-05-04 12:18:03 -1000

    adds factory

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,12 +4,15 @@
   var myApp = angular.module('myApp');
 
   //$scope in array format is necessary if minifying this file
-  myApp.controller('myController', ['$scope', 'MainCharacter', function($scope, MainCharacter){
+  myApp.controller('myController', ['$scope', 'MainCharacter', 'CharacterVersionFactory',
+    function($scope, MainCharacter, CharacterVersionFactory){
     //attach model to scope
     $scope.myFirstName = 'Owen';
 
     $scope.myModel = 'Mochi';
 
     $scope.mainCharacter = MainCharacter;
+
+    $scope.characterVersion = CharacterVersionFactory;
   }]);
 })();
---
+++ public/js/factories.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function(){
+  angular.module('myApp')
+    .factory('CharacterVersionFactory',
+      ['MainCharacter', 'APP_VERSION',
+      function(MainCharacter, APP_VERSION) {
+        return MainCharacter + " " + APP_VERSION;
+      }]);
+})();

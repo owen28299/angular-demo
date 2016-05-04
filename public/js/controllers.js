c74a6a775ef22c90206a6616644eb019b93583aa
Owen <owenyangg@gmail.com>
2016-05-04 11:23:31 -1000

    creates an injectable value ni controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,15 @@
 'use strict';
 
-//$scope in array format is necessary if minifying this file
-myApp.controller('myController', ['$scope', function($scope){
-  //attach model to scope
-  $scope.myFirstName = 'Owen';
+(function(){
+  var myApp = angular.module('myApp');
 
-  $scope.myModel = 'Mochi';
-}]);
+  //$scope in array format is necessary if minifying this file
+  myApp.controller('myController', ['$scope', 'MainCharacter', function($scope, MainCharacter){
+    //attach model to scope
+    $scope.myFirstName = 'Owen';
+
+    $scope.myModel = 'Mochi';
+
+    $scope.MainCharacter = MainCharacter;
+  }]);
+})();
--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,16 +1,18 @@
 'use strict';
 
-//creation uses a 2nd array argument to import dependencies.
-angular.module('myApp', []);
+(function(){
+  //creation uses a 2nd array argument to import dependencies.
+  angular.module('myApp', []);
 
-//this is retrieval - it will create myApp
-var myApp = angular.module('myApp');
+  //this is retrieval - it will create myApp
+  var myApp = angular.module('myApp');
 
-myApp
-  .config(function(){
-    //configuration
-  })
-  .run(function(){
-    //initialize
-  });
+  myApp
+    .config(function(){
+      //configuration
+    })
+    .run(function(){
+      //initialize
+    });
+})();
 

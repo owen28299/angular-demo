1fafd1a5f70edd13fa8a7eee28def2ca39aa92c5
Owen <owenyangg@gmail.com>
2016-05-04 11:12:40 -1000

    links to index.html

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -14,10 +14,3 @@ myApp
     //initialize
   });
 
-//$scope in array format is necessary if minifying this file
-myApp.controller('myController', ['$scope', function($scope){
-  //attach model to scope
-  $scope.myFirstName = 'Owen';
-
-  $scope.myModel = 'Mochi';
-}]);

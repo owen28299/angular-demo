3d430192fa82293a45636816c3d930ff23c8e046
Owen <owenyangg@gmail.com>
2016-05-04 19:03:56 -1000

    adds 404, removes #

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -2,7 +2,7 @@
 
 (function(){
   //creation uses a 2nd array argument to import dependencies.
-  angular.module('myApp', ['ngRoute']);
+  angular.module('myApp', ['ngRoute', 'ngAnimate']);
 
   //this is retrieval - it will create myApp
   var myApp = angular.module('myApp');
@@ -10,9 +10,15 @@
   myApp
     .config(['MoviesProvider',
       '$routeProvider',
-    function(MoviesProvider, $routeProvider){
+      '$locationProvider',
+    function(MoviesProvider, $routeProvider, $locationProvider ){
       //configuration
       MoviesProvider.setEndpoint('/api/movies');
+
+      $locationProvider.html5Mode({
+         enabled: true,
+         requireBase: false
+      });
 
       $routeProvider
         .when('/', {
@@ -29,6 +35,9 @@
         .when('/other', {
           templateUrl : 'views/other.html',
           controller  : 'OtherController'
+        })
+        .otherwise({
+          templateUrl : 'views/404.html'
         });
     }])
     .run(['$rootScope',
--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const express     = require('express'),
 app.use(express.static('public'));
 app.use("/api/movies", moviesRoute);
 
+app.get('*', function(req, res){
+  res.sendFile('./public/index.html',
+              {
+                root  : __dirname
+              });
+});
 
 app.listen(PORT, function(){
   console.log(`server listening on ${PORT}`);
